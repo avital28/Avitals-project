e2e63d115d045a003de629e4788e92ed06e69e82
User <User@LAB2-2>
2023-11-08 13:15:30 +0200

    updated

--- Avitals project/Models/User.cs
+++ Avitals project/Models/User.cs
@@ -10,9 +10,9 @@ namespace Avitals_project.Models
 {
     public class User
     {
-
-         public string Firstname { get; set; }  
-         public string Lastname { get; set; }
+        public  string Id { get; set; }
+        public string Firstname { get; set; }  
+        public string Lastname { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
