0de11a99bd329f076285a56edbead742590bb42c
User <User@LAB3-3>
2024-01-04 12:31:50 +0200

    updated

--- Avitals project/Views/UserDetailsPage.xaml.cs
+++ Avitals project/Views/UserDetailsPage.xaml.cs
@@ -1,9 +1,12 @@
+using Avitals_project.ViewModels;
+
 namespace Avitals_project.Views;
 
 public partial class UserDetailsPage : ContentPage
 {
-	public UserDetailsPage()
+	public UserDetailsPage( UserDetailsPageViewModel vm)
 	{
+		this.BindingContext = vm;	
 		InitializeComponent();
 	}
 }
